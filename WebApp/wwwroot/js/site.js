8dbce6c13e6ea056322134d6a3f0b5b056a3a1c6
Piotr Kochanowski <kohan142011@gmail.com>
2020-04-22 18:04:56 +0200

    Added hamburger mentu to navbar, changed colors

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+const toggleButton = document.getElementsByClassName('toggle__button')[0]
+const navbarLinks = document.getElementsByClassName('nav__links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
